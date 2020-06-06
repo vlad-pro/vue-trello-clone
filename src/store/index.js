7448b07f150c9525f792998fdc0366cd80c7006e
vproskurin <vladimir.proskurin@gmail.com>
2020-06-06 13:58:16 -0700

    Added ability to edit and add tasks

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import defaultBoard from '@/default-board.js'
-import { saveStatePlugin } from '@/utils.js'
+import { saveStatePlugin, uuid } from '@/utils.js'
 
 Vue.use(Vuex)
 
@@ -12,7 +12,19 @@ export default new Vuex.Store({
   state: {
     board
   },
-  mutations: {},
+  mutations: {
+    CREATE_TASK(state, { tasks, name }) {
+      tasks.push({
+        name,
+        id: uuid(),
+        description: ''
+      })
+    },
+    UPDATE_TASK(state, { task, key, value }) {
+      task[key] = value
+      // Vue.set(task, key, value)
+    }
+  },
   actions: {},
   modules: {},
   getters: {

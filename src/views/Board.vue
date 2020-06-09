847578e79b247792621a5edd74b2a988a0fcba33
vproskurin <vladimir.proskurin@gmail.com>
2020-06-08 22:51:16 -0700

    moving columns around

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -5,9 +5,11 @@
         class="column"
         v-for="(column, $columnIndex) of board.columns"
         :key="$columnIndex"
-        @drop="moveTask($event, column.tasks)"
+        draggable
+        @drop="moveTaskorColumn($event, column.tasks, $columnIndex)"
         @dragover.prevent
         @dragenter.prevent
+        @dragstart.self="pickupColumn($event, $columnIndex)"
       >
         <div class="flex items-center mb-2 font-bold">
           {{ column.name }}
@@ -77,6 +79,22 @@ export default {
 
       e.dataTransfer.setData('task-index', taskIndex)
       e.dataTransfer.setData('from-column-index', fromColumnIndex)
+      e.dataTransfer.setData('type', 'task')
+    },
+    pickupColumn(e, fromColumnIndex) {
+      e.dataTransfer.effectAllowed = 'move'
+      e.dataTransfer.dropEffect = 'move'
+
+      e.dataTransfer.setData('from-column-index', fromColumnIndex)
+      e.dataTransfer.setData('type', 'column')
+    },
+    moveTaskorColumn(e, toTasks, tocolumnIndex) {
+      const type = e.dataTransfer.getData('type')
+      if (type === 'task') {
+        this.moveTask(e, toTasks)
+      } else {
+        this.moveColumn(e, tocolumnIndex)
+      }
     },
     moveTask(e, toTasks) {
       const fromColumnIndex = e.dataTransfer.getData('from-column-index')
@@ -88,6 +106,14 @@ export default {
         fromTasks,
         toTasks,
         taskIndex
+      })
+    },
+    moveColumn(e, tocolumnIndex) {
+      const fromColumnIndex = e.dataTransfer.getData('from-column-index')
+
+      this.$store.commit('MOVE_COLUMN', {
+        fromColumnIndex,
+        tocolumnIndex
       })
     }
   }

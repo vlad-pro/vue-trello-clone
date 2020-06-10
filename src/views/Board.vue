0e4192b6126d211e593bd230a5a8230b59bc7aae
vproskurin <vladimir.proskurin@gmail.com>
2020-06-10 09:48:02 -0700

    Created functionality for adding a new column

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -46,6 +46,15 @@
           />
         </div>
       </div>
+      <div class="column flex">
+        <input
+          class="p-2 mr-2 flex-grow"
+          placeholder="New Column Name"
+          type="text"
+          v-model="newColumnnName"
+          @keyup.enter="createColumn"
+        />
+      </div>
     </div>
 
     <div class="task-bg" v-if="isTaskOpen" @click.self="close">
@@ -58,6 +67,11 @@
 import { mapState } from 'vuex'
 
 export default {
+  data() {
+    return {
+      newColumnnName: ''
+    }
+  },
   computed: {
     ...mapState(['board']),
     isTaskOpen() {
@@ -77,6 +91,11 @@ export default {
         name: e.target.value
       })
       e.target.value = ''
+    },
+    createColumn() {
+      this.$store.commit('CREATE_COLUMN', { name: this.newColumnnName })
+
+      this.newColumnnName = ''
     },
     pickupTask(e, taskIndex, fromColumnIndex) {
       e.dataTransfer.effectAllowed = 'move'

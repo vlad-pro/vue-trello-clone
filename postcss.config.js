afcfa25aaf42c331a526faebd21ddd0e84967962
vproskurin <vladimir.proskurin@gmail.com>
2020-06-11 19:30:27 -0700

    postcss config take 3 for prod. This time it works on my machine

--- postcss.config.js
+++ postcss.config.js
@@ -1,50 +1,50 @@
-const IN_PRODUCTION = process.env.NODE_ENV === 'production'
+// const IN_PRODUCTION = process.env.NODE_ENV === 'production'
 
-class TailwindVueExtractor {
-  static extract(content) {
-    const contentWithoutStyleBlocks = content.replace(
-      /<style[^]+?<\/style>/gi,
-      ''
-    )
-    return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_:/]+/g) || []
-  }
-}
+// class TailwindVueExtractor {
+//   static extract(content) {
+//     const contentWithoutStyleBlocks = content.replace(
+//       /<style[^]+?<\/style>/gi,
+//       ''
+//     )
+//     return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_:/]+/g) || []
+//   }
+// }
 
-const extensionsUsingCSS = ['vue', 'html']
-const extensionsOfCSS = [
-  'css',
-  'less',
-  'pcss',
-  'postcss',
-  'sass',
-  'scss',
-  'styl'
-]
+// const extensionsUsingCSS = ['vue', 'html']
+// const extensionsOfCSS = [
+//   'css',
+//   'less',
+//   'pcss',
+//   'postcss',
+//   'sass',
+//   'scss',
+//   'styl'
+// ]
 
-module.exports = {
-  plugins: [
-    require('postcss-preset-env')({ stage: 2 }),
-    require('tailwindcss')('./tailwind.config.js'),
-    IN_PRODUCTION &&
-      require('@fullhuman/postcss-purgecss')({
-        content: [`./@(public|src)/**/*.@(${extensionsUsingCSS.join('|')})`],
-        css: [`./src/**/*.@(${extensionsOfCSS.join('|')})`],
-        extractors: [
-          {
-            extractor: TailwindVueExtractor,
-            extensions: extensionsUsingCSS
-          }
-        ],
-        whitelist: [],
-        whitelistPatterns: [
-          /-(leave|enter|appear)(|-(to|from|active))$/,
-          /^(?!(|.*?:)cursor-move).+-move$/,
-          /^router-link(|-exact)-active$/
-        ]
-      }),
-    require('autoprefixer')()
-  ]
-}
+// module.exports = {
+//   plugins: [
+//     require('postcss-preset-env')({ stage: 2 }),
+//     require('tailwindcss')('./tailwind.config.js'),
+//     IN_PRODUCTION &&
+//       require('@fullhuman/postcss-purgecss')({
+//         content: [`./@(public|src)/**/*.@(${extensionsUsingCSS.join('|')})`],
+//         css: [`./src/**/*.@(${extensionsOfCSS.join('|')})`],
+//         extractors: [
+//           {
+//             extractor: TailwindVueExtractor,
+//             extensions: extensionsUsingCSS
+//           }
+//         ],
+//         whitelist: [],
+//         whitelistPatterns: [
+//           /-(leave|enter|appear)(|-(to|from|active))$/,
+//           /^(?!(|.*?:)cursor-move).+-move$/,
+//           /^router-link(|-exact)-active$/
+//         ]
+//       }),
+//     require('autoprefixer')()
+//   ]
+// }
 
 // const purgecss = require('@fullhuman/postcss-purgecss')({
 //   content: [
@@ -66,3 +66,15 @@ module.exports = {
 //     ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
 //   ]
 // }
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    process.env.NODE_ENV === 'production' &&
+      require('@fullhuman/postcss-purgecss')({
+        content: ['./src/**/*.vue', './public/index.html'],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+      })
+  ]
+}
